bb5c83bf460a837f1665a2c02ebe81d1f5a44d48
muscaiu <muscaiu@yahoo.com>
2016-08-15 20:20:30 +0300

    Improved Subscribing and a Loading...

--- client/recipes/recipes.js
+++ client/recipes/recipes.js
@@ -1,9 +1,17 @@
-Meteor.subscribe('recipes');
+//subscribe only to what need to be fetched for speed
+//Meteor.subscribe('recipes');
+
+Template.Recipes.onCreated(function(){
+    var self = this;
+    self.autorun(function(){ //unsubscribe from old subscriptions
+        self.subscribe('recipes');
+    })
+});
 
 Template.Recipes.helpers({
     recipes: () => {
         return Recipes.find({});
     }
-})
+});
 //console.log("access private: " + Meteor.settings.private.key);
 //console.log("access public: " + Meteor.settings.public.ga.account);
