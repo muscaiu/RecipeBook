15cbc5f809aa6092a8ed84515b77fa143949d5d5
muscaiu <muscaiu@yahoo.com>
2016-08-15 20:04:37 +0300

    Looping thourgh recipes with js and html

--- client/recipes/recipes.js
+++ client/recipes/recipes.js
@@ -1,4 +1,9 @@
 Meteor.subscribe('recipes');
 
+Template.Recipes.helpers({
+    recipes: () => {
+        return Recipes.find({});
+    }
+})
 //console.log("access private: " + Meteor.settings.private.key);
 //console.log("access public: " + Meteor.settings.public.ga.account);
